bdc2192098c05a86c6e187c1ae24f758127faf87
Nail Ismayilov <nailsmylv@gmail.com>
2022-06-21 17:46:20 +0400

    SignUp design

--- src/Components/users/SignIn.jsx
+++ src/Components/users/SignIn.jsx
@@ -6,7 +6,7 @@ const SignIn = () => {
 
   return (
    <div className="sign-container">
-     <div className='SignIn-container'>
+     <form className='SignIn-container'>
         <h2 className='signIn-head'>Sign In</h2>
         <div class="form__group field">
             <input required="" placeholder="Name" class="form__field" type="text" />
@@ -16,8 +16,11 @@ const SignIn = () => {
             <input required="" placeholder="Password" class="form__field" type="password" />
             <label class="form__label" for="name">Password</label>
         </div>
+
+      <button className='submitBtn' type='submit'>Submit</button>
+
         <a className='forgot-password' href="#">Forgot Password?</a>
-    </div>
+    </form>
    </div>
   )
 }
--- src/Components/users/SignUp.jsx
+++ src/Components/users/SignUp.jsx
@@ -2,14 +2,26 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>
-        <h1>SignUp</h1>
-        <h1>SignUp</h1>
-        <h1>SignUp</h1>
-        <h1>SignUp</h1>
-        <h1>SignUp</h1>
-        <h1>SignUp</h1>
-    </div>
+    <div className="sign-container">
+    <form className='SignIn-container'>
+       <h2 className='signIn-head'>Sign Up</h2>
+       <div class="form__group field">
+           <input required="" placeholder="Name" class="form__field" type="text" />
+           <label class="form__label" for="name">Username</label>
+       </div>
+       <div class="form__group field">
+           <input required="" placeholder="Name" class="form__field" type="text" />
+           <label class="form__label" for="name">Email</label>
+       </div>
+       <div class="form__group field">
+           <input required="" placeholder="Password" class="form__field" type="password" />
+           <label class="form__label" for="name">Password</label>
+       </div>
+
+     <button className='submitBtn' type='submit'>Submit</button>
+
+   </form>
+  </div>
   )
 }
 
